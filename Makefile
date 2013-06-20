d9efdb88176421ce93df0ceb8fdfc15a21e623a8
jkroso <jkroso@gmail.com>
2013-06-20 15:14:39 +1200

    update tests

--- Makefile
+++ Makefile
@@ -3,13 +3,18 @@ REPORTER=dot
 serve: node_modules
 	@node_modules/serve/bin/serve
 
-test:
-	@node_modules/.bin/mocha test/*.test.js \
+test: node_modules
+	@node_modules/mocha/bin/_mocha test/*.test.js \
 		--reporter $(REPORTER) \
+		--timeout 500 \
+		--check-leaks \
 		--bail
 
-node_modules: package.json
-	@packin install -m package.json -edr
+node_modules: component.json
+	@packin install --meta deps.json,component.json,package.json \
+		--folder node_modules \
+		--executables \
+		--no-retrace
 
 clean:
 	rm -r node_modules
--- test/map.test.js
+++ test/map.test.js
@@ -1,36 +1,35 @@
 
-var should = require('chai').should()
-  , promise = require('laissez-faire')
+var chai = require('./chai')
   , series = require('../series')
   , async = require('../async')
+  , Result = require('result')
   , map = require('..')
 
-function delay(err, val){
-	var args = arguments
-	return promise(function(fulfill, reject){
-		setTimeout(function () {
-			if (args.length < 2) reject(err)
-			else fulfill(val)
-		}, Math.random() * 10)
-	})
+function delay(value){
+	var result = new Result
+	setTimeout(function () {
+		if (value instanceof Error) result.error(value)
+		else result.write(value)
+	}, Math.random() * 10)
+	return result
 }
 
-describe('map', function () {
-	it('should work on arrays', function () {
+describe('map', function(){
+	it('should work on arrays', function(){
 		map([1,2,3], function(v,k){
 			k.should.be.a('number')
 			return v + 1
 		}).should.eql([2,3,4])
 	})
 
-	it('should work on objects', function () {
+	it('should work on objects', function(){
 		map({a:1, b:2, c:3}, function(v,k){
 			k.should.be.a('string')
 			return v + 1
 		}).should.eql({a:2,b:3,c:4})
 	})
 
-	it('should call `fn` with `ctx` as `this`', function () {
+	it('should call `fn` with `ctx` as `this`', function(){
 		map([1,2,3], function(v, k){
 			this.should.equal(Math)
 			return Math.pow(v, 2)
@@ -38,12 +37,12 @@ describe('map', function () {
 	})
 })
 
-describe('series', function () {
-	it('should work on arrays', function (done) {
+describe('series', function(){
+	it('should work on arrays', function(done){
 		var calls = []
 		series([1,2,3], function(v, k){
 			k.should.be.a('number')
-			return delay(null, v + 1).read(function(){
+			return delay(v + 1).read(function(){
 				calls.push([k, v])
 			})
 		}).then(function(res){
@@ -56,13 +55,13 @@ describe('series', function () {
 		}).node(done)
 	})
 
-	it('should work on objects', function (done) {
+	it('should work on objects', function(done){
 		var calls = []
 		var delayed = []
 		series({a:1,b:2,c:3}, function(v, k){
 			k.should.be.a('string')
 			calls.push([k, v])	
-			return delay(null, v + 1).read(function(){
+			return delay(v + 1).read(function(){
 				delayed.push([k, v])
 			})
 		}).then(function(res){
@@ -71,14 +70,14 @@ describe('series', function () {
 		}).node(done)
 	})
 
-	it('should call `fn` with `ctx` as `this`', function (done) {
+	it('should call `fn` with `ctx` as `this`', function(done){
 		series([1,2,3], function(v, k){
 			this.should.equal(Math)
-			return delay(null, null)
+			return delay()
 		}, Math).node(done)
 	})
 
-	it('should handle empty arrays', function (done) {
+	it('should handle empty arrays', function(done){
 		series([], function(v, k){
 			throw new Error('fail')
 		}).then(function(res){
@@ -86,7 +85,7 @@ describe('series', function () {
 		}).node(done)
 	})
 
-	it('should handle empty objects', function (done) {
+	it('should handle empty objects', function(done){
 		series({}, function(v, k){
 			throw new Error('fail')
 		}).then(function(res){
@@ -100,7 +99,7 @@ describe('series', function () {
 describe('async', function(){
 	it('should work on arrays', function(done){
 		async([1,2,3], function(v, i){
-			return delay(null, v + 1)
+			return delay(v + 1)
 		}).then(function(arr){
 			arr.should.eql([2,3,4])
 		}).node(done)
@@ -108,7 +107,7 @@ describe('async', function(){
 
 	it('should work on objects', function(done){
 		async({a:1,b:2,c:3}, function(v, i){
-			return delay(null, v + 1)
+			return delay(v + 1)
 		}).then(function(obj){
 			obj.should.eql({a:2,b:3,c:4})
 		}).node(done)
@@ -118,20 +117,20 @@ describe('async', function(){
 })
 
 function errorHandling(map){
-	describe('error handling', function () {
-		it('should catch sync errors', function (done) {
+	describe('error handling', function(){
+		it('should catch sync errors', function(done){
 			map([1,2,3], function(v, k){
 				if (v == 2) throw new Error('fail')
-				return delay(null, v + 1)
+				return delay(v + 1)
 			}).then(null, function(e){
 				e.message.should.equal('fail')
 			}).node(done)
 		})
 
-		it('should catch async errors', function (done) {
+		it('should catch async errors', function(done){
 			map([1,2,3], function(v, k){
 				if (v == 2) return delay(new Error('fail'))
-				return delay(null, null)
+				return delay()
 			}).then(null, function(e){
 				e.message.should.equal('fail')
 				done()
